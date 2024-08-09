4759405e84fd4e813f51f3faf633d4f107da5516
Whatever.org <whateverdotorg18@gmail.com>
2024-08-09 14:52:24 +0200

    Branch errors

--- AetherUnit/src/main.cpp
+++ AetherUnit/src/main.cpp
@@ -1,17 +1,5 @@
 #include <Arduino.h>
 
-<<<<<<< HEAD
-//Blinking_LED
-
-//turn on the LED for half a second,then off for half a second,repeatedly
-
-//info@primerobotics.in
-
-//www.primerobotics.in
-
-/************************************************/
-=======
->>>>>>> 203ac4a745e1a6199248e677533474e208d76a3f
 
 const int ledPin = 9;//the number of the LED pin
 
@@ -26,11 +14,7 @@ pinMode(ledPin,OUTPUT);//initialize the digital pin as an output
 
 }
 
-<<<<<<< HEAD
-/************************************************/
-=======
 //************************************************
->>>>>>> 203ac4a745e1a6199248e677533474e208d76a3f
 
 //the loop routine runs over and over again forever
 
@@ -42,24 +26,14 @@ void loop()
 digitalWrite(ledPin,HIGH);//turn the LED on 
 
  
-<<<<<<< HEAD
-delay(1000);               //wait
-//for half a second
-=======
 delay(500);               //wait
 
->>>>>>> 203ac4a745e1a6199248e677533474e208d76a3f
 
  
 digitalWrite(ledPin,LOW); //turn the LED off
 
  
 delay(500);               //wait
-<<<<<<< HEAD
-//for half a second
-=======
-
->>>>>>> 203ac4a745e1a6199248e677533474e208d76a3f
 
 }
 
