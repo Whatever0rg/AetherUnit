e2302fbdc1e21d5062608f9bebb4acd3f1bd5aca
Whatever.org <whateverdotorg18@gmail.com>
2024-08-10 17:27:10 +0200

    BME280 and CCS811 write data to Serial

--- AetherUnit/src/main.cpp
+++ AetherUnit/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Adafruit_CCS811.h>
+#include <Adafruit_BME280.h>
 
-
+Adafruit_CCS811 ccs;
+Adafruit_BME280 bme;
 
 const int ledPin = 9;//the number of the LED pin
 const int chipSelect = 10; // telling the Board where the microSD card is
@@ -12,9 +15,10 @@ File testFile;
 
 
 void setup()
-
 {
-Serial.begin(9600);                             //Initializing the microSD card
+Serial.begin(9600);
+SPI.begin();
+  //Initializing the microSD card                          
   Serial.print("Initializing SD card..."); 
   if (!SD.begin()) {
     Serial.println("initialization failed!");
@@ -32,10 +36,6 @@ testFile = SD.open("test.txt", FILE_WRITE);
 } else {
     Serial.println("error opening test.txt");
 }
-
-pinMode(ledPin,OUTPUT);//initialize the digital pin as an output
-
-
 testFile = SD.open("test.txt");
 
 if (testFile) {
@@ -47,25 +47,63 @@ if (testFile) {
 } else {
     Serial.println("error opening test.txt");
 }
+
+// CCS811 Startup
+
+  Serial.println("CCS811 test");
+
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+
+  // Wait for the sensor to be ready
+  while(!ccs.available());
+
+// BME Setup
+bool status;
+status = bme.begin(0x76);  
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);}
+
+// LED Setup
+pinMode(ledPin,OUTPUT);
+
 }
+
 //the loop routine runs over and over again forever
 
 void loop()
-
 {
+  digitalWrite(ledPin,HIGH);//turn the LED on 
 
  
-digitalWrite(ledPin,HIGH);//turn the LED on 
+  delay(500);               //wait
+
+  if(ccs.available()){
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+
+  auto temperature = String(bme.readTemperature()) + " °C";
+  auto pressure = String(bme.readPressure() / 100.0) + " hPa";
+  auto humidity = String(bme.readHumidity()) + " %";
+
+  String bmeMeasurements = temperature + ", " + pressure + ", " + humidity;
+  Serial.println(bmeMeasurements);
+
+  digitalWrite(ledPin,LOW); //turn the LED off
 
  
-delay(500);               //wait
-
-
- 
-digitalWrite(ledPin,LOW); //turn the LED off
-
- 
-delay(500);               //wait
+  delay(500);               //wait
 
 }
-
